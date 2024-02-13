582bf53feeb983af8801d0d452e98706d9dbe42e
mahnsi <mahnsir22@gmail.com>
2024-02-13 12:34:40 -0500

    first commit

---
+++ lastfmscript.js
@@ -0,0 +1,99 @@
+//top 10 arrists
+//if month is selected, one set a day - past 30 days or can select a specific month
+//if year is selected, show data by month (one set per month)
+//all time- by month and year (one set a month)
+//for custom, just have a year selection or month of the year selection
+
+const APIkey = "976a55f9469d78ad881fc3daa33cf5b3";
+
+document.addEventListener('DOMContentLoaded', function() {
+  const formContainer = document.getElementById('form_container');
+  const getBackButton = document.getElementById('again_button');
+  const toggleButton = document.getElementById('submit_button');
+  const myForm = document.getElementById('start_form');
+
+  //if the form is visible, then the back to form button is not visible
+  if (formContainer.style.display != 'none') {
+    getBackButton.style.display ='none';
+  } 
+
+  // Triggered when form is submitted 
+  function toggleForm() {
+    if (formContainer.style.display === 'none') {
+      formContainer.style.display = 'block';
+      getBackButton.style.display ='none';
+      
+    } else {
+      //submit has been pressed. the form is visible. make invisible.
+      formContainer.style.display = 'none';
+      getBackButton.style.display ='block';
+      getBackButton.textContent = 'Redo';
+    }
+  }
+
+  // Event listener for form submission
+  myForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    const username = document.getElementById('username').value;
+    const frame = document.getElementById('time_frame').value;
+    console.log("Entered user: ", username);
+    console.log("Entered Time Frame: ", frame);
+    toggleForm(); 
+    myForm.reset(); // Clear form fields
+    getEpoch(username);
+    getDataSet(username, frame);
+  });
+
+  // Event listener for redo click to toggle form visibility
+  getBackButton.addEventListener('click', toggleForm);
+});
+
+function getEpoch(username){
+  const URL = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user="+username+"&api_key="+APIkey+"&format=json";
+  fetch(URL)
+    .then(response=>{
+      if(!response.ok){
+        throw new Error('Network error');
+      }
+      return response.json();
+    })
+    .then(data=>{
+      const user = data.user;
+      const epoch = user.registered.unixtime;
+      const date = new Date(epoch * 1000); // Convert seconds to milliseconds
+      console.log(date);
+      return epoch;
+    })
+    .catch(error=>{
+      console.error('Operation error', error);
+    });
+
+}
+
+function getDataSet(username, period){
+    const URL = "http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user="
+    +username+"&api_key="+
+    APIkey+"&format=json";
+    //list of top artists
+    //corresponding list of numbers
+    //n amount these 2 lists, n being number of months, n=15 when 1 month is selected (every 2 days)
+    fetch(URL)
+    .then(response=>{
+      if(!response.ok){
+        throw new Error('Network error');
+      }
+      return response.json();
+    })
+    .then(data=>{
+      const chart = data.weeklyartistchart;
+      const artists = chart.artist;
+      for(artist of artists){
+        console.log(artist.name);      
+      }
+      //console.log(data);
+    })
+    .catch(error=>{
+      console.error('Operation error', error);
+    });
+}
+
